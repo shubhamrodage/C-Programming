128e6f4a950799f60a55fb77cc1397cde6fb3083
shubhamrodagetech <rodageshubham756@gmail.com>
2026-06-18 09:29:47 +0530

    Add files via upload

---
+++ Practice/LB Class/program4.c
@@ -0,0 +1,22 @@
+// addition of two numbers 
+#include<stdio.h>
+
+int main()
+{
+    int i,j,k;
+
+    printf("Enter first number : \n");
+    scanf("%d",&i);
+
+    printf("Enter second number : \n");
+    scanf("%d",&j);
+
+    // bussiness logic
+
+    k = i + j;               // = assignment operator
+
+    printf("Addition is : %d\n",k);
+
+    return 0;
+}
+
