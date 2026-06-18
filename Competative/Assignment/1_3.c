20ab9343de16e91b89781c4a5afddeba6c85b175
shubhamrodagetech <rodageshubham756@gmail.com>
2026-06-18 09:26:32 +0530

    Add files via upload

---
+++ Competative/Assignment/1_3.c
@@ -0,0 +1,20 @@
+// Print number 5 to 1
+#include<stdio.h>
+
+void Display()
+{
+    int i = 0;
+    i = 5;
+    while(i>=1)
+    {
+        printf("%d",i);
+        i--;
+    }
+}
+
+int main()
+{
+    Display();
+
+    return 0;
+}
