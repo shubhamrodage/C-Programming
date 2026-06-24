ff75794da18066290090f165438cfd7f7baac548
shubhamrodagetech <rodageshubham756@gmail.com>
2026-06-24 19:42:52 +0530

    Add files via upload

---
+++ Practice/LB Class/program24.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main()
+{
+    // Sequesce
+    
+    printf("Jay Ganesh...\n");
+    printf("Jay Ganesh...\n");
+    printf("Jay Ganesh...\n");
+    printf("Jay Ganesh...\n");
+
+    return 0;
+}
